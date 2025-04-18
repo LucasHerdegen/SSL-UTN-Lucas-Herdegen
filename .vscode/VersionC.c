36b08219eeacdc7443c9c87f5ffb31cff77c17bf
LucasHerdegen <lherdegen@frba.utn.edu.ar>
2025-04-18 13:51:03 -0300

    Modificacion .gitignore

--- .vscode/VersionC.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Versión de C: %ld\n", __STDC_VERSION__);
-    return 0;
-}
--- TP_0.c
+++ TP_0.c
@@ -3,6 +3,5 @@
 int main (void)
 {
     printf("Hello, world! (Hola, mundo! para los que no saben ingles)");
-    printf("");
     return 0;
 }
